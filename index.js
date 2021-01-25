966e47aa88f5c9df9cbe0fe4889d9c908ead3814
Aarón Montes <aaron@kssco.com>
2021-01-25 00:40:33 -0600

    Updating individual exporting of components on index.js v4

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import 'mini.css';
 
-import mHeader from '../src/components/Header/m-header';
-import mDrawer from '../src/components/Drawer/m-drawer';
-import mSpinner from '../src/components/Spinner/m-spinner';
-import mProgressBar from '../src/components/ProgressBar/m-progress-bar';
-import mHighLight from '../src/components/Highlight/m-highlight';
-import mTooltip from '../src/components/Tooltip/m-tooltip';
-import mButton from '../src/components/Buttons/m-button';
-import mToast from '../src/components/Toast/m-toast';
-import MFieldset from '../src/components/Inputs/m-fieldset';
-import MIcon from '../src/components/Icons/m-icon';
-import MDropdown from '../src/components/Dropdown/m-dropdown';
-import MTable from '../src/components/Table/m-table';
-import MAccordion from '../src/components/Accordion/m-accordion';
-import MInput from '../src/components/Inputs/m-input';
-import MTabs from '../src/components/Tabs/m-tabs';
+import mHeader from './src/components/Header/m-header';
+import mDrawer from './src/components/Drawer/m-drawer';
+import mSpinner from './src/components/Spinner/m-spinner';
+import mProgressBar from './src/components/ProgressBar/m-progress-bar';
+import mHighLight from './src/components/Highlight/m-highlight';
+import mTooltip from './src/components/Tooltip/m-tooltip';
+import mButton from './src/components/Buttons/m-button';
+import mToast from './src/components/Toast/m-toast';
+import MFieldset from './src/components/Inputs/m-fieldset';
+import MIcon from './src/components/Icons/m-icon';
+import MDropdown from './src/components/Dropdown/m-dropdown';
+import MTable from './src/components/Table/m-table';
+import MAccordion from './src/components/Accordion/m-accordion';
+import MInput from './src/components/Inputs/m-input';
+import MTabs from './src/components/Tabs/m-tabs';
 
 export default  {
   install(Vue) {
