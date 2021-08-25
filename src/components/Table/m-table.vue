3c4fbf2419b0d0ed3beb5e49115177cc107d4c73
ajomuch92 <aaronmontes_92@yahoo.es>
2021-08-25 12:05:14 -0600

    Updating m-table component to use master-detail, downloadble and pagination for this one

--- src/components/Table/m-table.vue
+++ src/components/Table/m-table.vue
@@ -6,6 +6,9 @@
       </span>
       <span>
         <slot name="search"></slot>
+        <span v-if="downloadable" class="download-icon" @click="downloadContent">
+          <m-icon name="download-cloud"/>
+        </span>
       </span>
     </div>
     <table :class="getClasses">
@@ -19,21 +22,69 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(item, key) in originalItems" :key="key" @click="itemClick(item)" :class="{'selected': item==selectedItem}">
-          <td v-for="(header, key2) in headers" :key="key2" :data-label="header">
-            <slot :name="`row-${header}`">
-              {{item[header]}}
-            </slot>
-          </td>
-        </tr>
-        <div class="no-items" v-if="originalItems.length==0">{{noItemsText}}</div>
+        <template v-if="!masterDetail">
+          <tr v-for="(item, key) in originalItems" :key="key" @click="itemClick(item)" :class="{'selected': item==selectedItem}">
+            <td v-for="(header, key2) in headers" :key="key2" :data-label="header">
+              <slot :name="`row-${header}`">
+                {{item[header]}}
+              </slot>
+            </td>
+          </tr>
+          <div class="no-items" v-if="originalItems.length==0">{{noItemsText}}</div>
+        </template>
+        <template v-else>
+          <template v-for="key in (originalItems.length*2)">
+            <tr v-if="key%2==1" :key="key" @click="itemClick(originalItems[(key-1)/2])" :class="{'selected': originalItems[(key-1)/2]==selectedItem}">
+              <td v-for="(header, key2) in headers" :key="key2" :data-label="header">
+                <span class="expand-icon" v-if="key2==0" @click="expandeRow(key+1)">
+                  <m-icon height="12" width="12" :name="expandedRowIndex==(key+1)?'chevron-down': 'chevron-right'"/>
+                </span>
+                <slot :name="`row-${header}`">
+                  {{originalItems[(key-1)/2][header]}}
+                </slot>
+              </td>
+            </tr>
+            <tr v-else :key="key" v-show="expandedRowIndex==key">
+              <td :colspan="headers.length">
+                <slot name="details" v-bind:item="originalItems[key/2-1]">Detail</slot>
+              </td>
+            </tr>
+          </template>
+          <div class="no-items" v-if="originalItems.length==0">{{noItemsText}}</div>
+        </template>
       </tbody>
     </table>
+    <m-row>
+      <m-col lg="3" md="6" sm="12" lg-offset="9" md-offset="6" sm-offset="0">
+        <m-button v-if="numberOfPages>5" class="button-pagination" :hover-effect="false" size="small" :width="35" @click="moveBack">
+          <m-icon height="15" width="15" name="chevron-left" />
+        </m-button>
+        <span v-if="currentPage>4&&currentPage>0">...</span>
+        <m-button
+          v-for="index in 5"
+          :key="index"
+          class="button-pagination"
+          :class="{'bordered': (index-1+deltaPage)==currentPage}"
+          :hover-effect="false"
+          size="small"
+          @click="currentPage=index+deltaPage-1"
+          :width="35">{{index+deltaPage}}</m-button>
+        <span v-if="numberOfPages>5&&currentPage+1<numberOfPages">...</span>
+        <m-button v-if="numberOfPages>5" class="button-pagination" :hover-effect="false" size="small" :width="35" @click="moveForward">
+          <m-icon height="15" width="15" name="chevron-right" />
+        </m-button>
+      </m-col>
+    </m-row>
   </div>
 </template>
 
 <script>
+import MButton from '../Buttons/m-button.vue';
+import MCol from '../Grid/m-col.vue';
+import MRow from '../Grid/m-row.vue';
+import mIcon from '../Icons/m-icon.vue';
 export default {
+  components: { mIcon, MRow, MCol, MButton },
   name: 'm-table',
   props: {
     headers: {
@@ -71,12 +122,28 @@ export default {
     noItemsText: {
       type: String,
       default: 'Sin datos para mostrar'
+    },
+    masterDetail: {
+      type: Boolean,
+      default: false
+    },
+    downloadable: {
+      type: Boolean,
+      default: false
+    },
+    itemsPerPage: {
+      type: Number,
+      default: -1
     }
   },
   data: () => ({
     selectedItem: {},
     originalItems: [],
-    searchItems: []
+    searchItems: [],
+    expandedRowIndex: -1,
+    deltaPage: 0,
+    currentPage: 0,
+    sortedBy: {}
   }),
   created() {
     this.setItems();
@@ -90,7 +157,12 @@ export default {
         'dense': this.dense,
         'selectable': this.selectable
       };
-    }
+    },
+    numberOfPages() {
+      if(this.itemsPerPage == -1)
+        return 1;
+      return Math.ceil(this.items.length / this.itemsPerPage);
+    },
   },
   watch: {
     search(){
@@ -98,11 +170,20 @@ export default {
     },
     items(){
       this.setItems();
+    },
+    currentPage(val) {
+      if(this.itemsPerPage != -1) {
+        const index = val * this.itemsPerPage;
+        this.originalItems = this.items.slice(index, index + this.itemsPerPage);
+      }
     }
   },
   methods: {
     setItems() {
-      this.originalItems = [...this.items];
+      if(this.itemsPerPage == -1)
+        this.originalItems = [...this.items];
+      else
+        this.originalItems = this.items.slice(0, this.itemsPerPage);
       this.searchItems = [...this.items];
     },
     itemClick(item){
@@ -129,12 +210,56 @@ export default {
       } else {
         this.originalItems = [...this.items];
       }
+      this.deltaPage = 0;
+      this.currentPage = 0;
+    },
+    expandeRow(index) {
+      if(this.expandedRowIndex == index) {
+        this.expandedRowIndex = -1;
+      } else {
+        this.expandedRowIndex = index;
+      }
+    },
+    downloadContent(){
+      let csv = '';
+      csv += this.headers.join(', ');
+      csv += '\n';
+      for(const item of this.items) {
+        csv += Object.values(item).join(', ');
+        csv += '\n';
+      }
+      const csvFile = new Blob([csv], {type: 'text/csv'});
+      const downloadLink = document.createElement('a');
+      const date = new Date();
+      downloadLink.download = `table_content_${date.getTime()}.csv`;
+      downloadLink.href = window.URL.createObjectURL(csvFile);
+      downloadLink.style.display = 'none';
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink)
+    },
+    moveForward() {
+      if(this.currentPage + 1 < this.numberOfPages){
+        this.currentPage++;
+        if(this.currentPage > 4 && this.currentPage < this.numberOfPages)
+          this.deltaPage++;
+      }
+    },
+    moveBack() {
+      if(this.currentPage > 0) {
+        this.currentPage--;
+        if(this.currentPage < this.numberOfPages - 5 && this.deltaPage != 0)
+          this.deltaPage--;
+      }
+    },
+    sortItems() {
+      
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
   .dense td, .dense th{
     padding: calc(var(--universal-padding) / 2) calc(var(--universal-padding)) !important;
   }
@@ -160,5 +285,17 @@ export default {
   .no-items{
     width: 100%;
     text-align: center;
+  }
+
+  .expand-icon {
+    cursor: pointer;
+  }
+  
+  .download-icon {
+    cursor: pointer;
+  }
+
+  .button-pagination {
+    margin: 10px 1px !important;
   }
 </style>
