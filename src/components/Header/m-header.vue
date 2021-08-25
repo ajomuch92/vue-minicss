26cac26601cd930c28c397e838c0184c11037da3
ajomuch92 <aaronmontes_92@yahoo.es>
2021-08-25 12:03:40 -0600

    Supporting responsiveness on header component

--- src/components/Header/m-header.vue
+++ src/components/Header/m-header.vue
@@ -1,29 +1,60 @@
 <template>
-  <header :class="isSticky">
+  <header ref="header" class="m-header m-flex m-align-center" :class="isSticky">
     <span class="logo">
       <slot name="logo"></slot>
     </span>
-    <slot name="default"></slot>
+    <slot v-if="!$size.isExtraSmall()" name="default"></slot>
+    <m-spacer/>
+    <m-drawer v-if="$size.isExtraSmall()" id="m-navbar">
+      <div class="m-flex m-flex-column" v-html="htmlDrawer"></div>
+    </m-drawer>
   </header>
 </template>
 
 <script>
+import MDrawer from '../Drawer/m-drawer.vue';
+import MSpacer from '../Grid/m-spacer.vue';
+
 export default {
   name: 'm-header',
+  components: {
+    MDrawer,
+    MSpacer
+  },
   props: {
     sticky: {
       type: Boolean,
       default: false
     },
   },
+  data: () => ({
+    children: []
+  }),
+  mounted() {
+    const {children} = this.$refs.header;
+    for(const element of children) {
+      if(!element.classList.contains('logo')) {
+        this.children.push(element);
+      }
+    }
+  },
   computed: {
     isSticky(){
       return this.sticky? 'sticky': '';
+    },
+    htmlDrawer() {
+      let html = '';
+      for(const element of this.children) {
+        html += element.outerHTML
+      }
+      return html;
     }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .m-header {
+    box-shadow: 1px 1px 2px #999;
+  }
 </style>
